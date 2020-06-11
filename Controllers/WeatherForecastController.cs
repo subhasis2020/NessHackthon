b04bfad8b7bfef1b9cf136b2e5da6a617e1f09ae
subhasis2020 <54503787+subhasis2020@users.noreply.github.com>
2020-06-11 11:15:40 +0530

    added controller

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Cities = new[]
+        {
+            "Mumbai", "Kolkata", "Bangalore", "Pune", "Chennai"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(30, 40),
+                City = Cities[rng.Next(Cities.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
